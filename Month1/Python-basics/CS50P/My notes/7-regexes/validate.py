e7f7e1c46f832aff6aaa730a5d45976a64733928
Sajedeh Arabali <arabalisajedeh@gamil.com>
2026-06-25 20:02:59 +0330

    modified:   Month1/Python-basics/CS50P/My notes/7-regexes/validate.py

--- Month1/Python-basics/CS50P/My notes/7-regexes/validate.py
+++ Month1/Python-basics/CS50P/My notes/7-regexes/validate.py
@@ -27,8 +27,21 @@ email = input("What is your email?: ").strip()
 # Checking if it ends with .com(we put a backslash to make sure the code does not mistake . of .com with the regex.)
 # r stands for raw and works like fstring
 # ^ is start of string and $ is end of string
-if re.search(r"^.+@.+\.com$", email):
+#if re.search(r"^.+@.+\.com$",email): validated even if there were 2@
+# if re.search(r"^[^@]+@[^@]+\.com$", email): Excludes @ 
+# \w instead of a-zA-Z0-9_
+# Flag = re.ignorecase
+# if re.search(r"^[\w]+@[\w]+\.com$",email,re.IGNORECASE):it does not support domains that contain 2 or more .s
+
+
+
+
+# The pattern explained:
+# ^\w+          -> Starts with one or more alphanumeric characters
+# @             -> A literal @ symbol
+# (\w+\.)+      -> Matches "something followed by a dot" one or more times (e.g., "sub." or "sub.domain.")
+# (com|edu|gov) -> Matches the final top-level domain
+if re.search(r"^(\w|\.)+@(\w+\.)+(com|edu|gov|org)$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
-
