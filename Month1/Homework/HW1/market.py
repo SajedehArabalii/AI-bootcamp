8d4294e4e92f726b00f3ece28701845b93112cb5
Sajedeh Arabali <arabalisajedeh@gamil.com>
2026-06-29 12:30:51 +0330

    Completed

--- Month1/Homework/HW1/market.py
+++ Month1/Homework/HW1/market.py
@@ -46,17 +46,20 @@ def main():
             case "exit":
                 print("Goodbye!")
                 return
+        print()
 
 #gets choice and makes sure it is valid
 def get_choice(prompt):
+    VALID_CHOICES = ("add","sell","search","show","save","report","exit")
     while True:
-        valid = ["add","sell","search","show","save","report","exit"]
+        #Because it is a constant so we use tuples
+
         choice = input(prompt).strip().lower()
-        if choice in valid:
+        if choice in VALID_CHOICES:
             return choice
         else:
             print("\tYour choice was invalid")
-
+            print()
 
 # add => gets name and amount, and adds them
 #   if the item existed before it should only add to the amount
@@ -64,91 +67,144 @@ def add(stock):
     while True:
 
         name = get_name("Name of the item: ")
-        amount = get_int("How many to stock:")
+        amount_to_add = get_int("How many to stock:")
 
-        for item, quantity in stock.items():
-            if item == name:
-                amount += quantity
-        
+        # for item, quantity in stock.items():
+        #     if item == name:
+        #         amount += quantity
+        # stock[name] = amount
 
-        stock[name] = amount
+        #.get():Give me this value if it exists, otherwise give me zero
+        stock[name] = stock.get(name, 0) + amount_to_add
         
         if input("Do you want to continue(y/n): ") != "y":
             break
-    
 
 # sell => gets name and amount, and reduct it from the stocks
 #   if there is not enough in stock print("not enough")  
 def sell(stock):
-    flag = 0
     name = get_name("What item did you sell: ")
-    sold = get_int("How many were sold: ")
-    for item, quantity in stock.items():
-        if item == name:
-            flag =1
-            if quantity >= sold:
-                stock[name] = quantity - sold
-            else:
-                print("\tNot enough in stock")
-    if flag == 0:
-        print("\tCould not find the item")
+    amount_to_sell = get_int("How many were sold: ")
+    # flag = 0
+    # for item, quantity in stock.items():
+    #     if item == name:
+    #         flag =1
+    #         if quantity >= sold:
+    #             stock[name] = quantity - sold
+    #         else:
+    #             print("\tNot enough in stock")
+    # if flag == 0:
+    #     print("\tCould not find the item")
 
-
-
+    #Getting rid of flag
+    #Getting rid of for
+    #Accessing every value using stock[name]
+    if name not in stock:
+        print("\tCould not find item.\n")
+        return
+    # if stock[name] >= amount_to_sell:
+    #     stock[name] -= amount_to_sell
+    #     remove_empty(stock,name)
+    # else:
+    #     print("\tNot enough.")
+    if stock[name] < amount_to_sell:
+        print("\tNot enough:")
+        return
+    stock[name] -= amount_to_sell
+    remove_empty(stock,name)
 
 # search => get name and say how many left
 def search(stock):
-    flag = 0
     name = get_name("Name of the item you are looking for: ")
-    for item, quantity in stock.items():
-        if item == name:
-            flag =1
-            print(f"\tYou have {quantity} in stock.")
-    if flag == 0:
+    # flag = 0
+    # for item, amount in stock.items():
+    #     if item == name:
+    #         flag =1
+    #         print(f"\tYou have {amount} in stock.")
+    # if flag == 0:
+    #     print("\tCould not find the item")
+    # print()
+
+    if name in stock:
+        print(f"\tYou have {stock[name]} in stock")
+    else:
         print("\tCould not find the item")
-
-
 
 # show => every item and quantity
 def show(stock):
-    for item, quantity in stock.items():
-        print(f"\t{item} - {quantity}")
+    if not stock:
+        print("\tNo items.")
+        return
+    for item, amount in stock.items():
+        print(f"\t{item} - {amount}")
+
 
 # save => save info in a txt
 # format => milk - 12
 def save(stock):
-    open("stock.txt",'w').close()
-    for item,quantity in stock.items():
-        with open("stock.txt","a") as file:
-            file.write(f"{item} - {quantity}\n")
+    #Opens file over and over which is bad
+    # open("stock.txt",'w').close()
+    # for item,quantity in stock.items():
+    #     with open("stock.txt","a") as file:
+    #         file.write(f"{item} - {quantity}\n")
 
+    with open("stock.txt","w") as file:
+        for item,amount in stock.items():
+            file.write(f"{item} - {amount}\n")
+    print("\tStock saved.")
+
+    
 
 # report => all of items in stock
 #           amount of items in stock
 #           item with the most quantity
 #           item with the least quantity
 def report(stock):
+    if not stock:
+        print("Stock is empty")
+        return
     show(stock)
+    amount_of_stuff(stock)
     most(stock)
     least(stock)
 
+
+def amount_of_stuff(stock):
+    print(f"\tYou have {sum(stock.values())} items in total.")
+
 def most(stock):
-    high = 0
-    for quantity in stock.values():
-        if high < quantity:
-            high = quantity
-    for item, quantity in stock.items():
-        if quantity == high:
-            print(f"You have the most amount of {item}")
+    # high = 0
+    # for quantity in stock.values():
+    #     if high < quantity:
+    #         high = quantity
+
+    #gets the highest amount of value
+    # high =  max(stock.values())
+
+    # for item, quantity in stock.items():
+    #     if quantity == high:
+    #         print(f"You have the most amount of {item}")
+
+    item = max(stock, key=stock.get)
+    print(f"\tYou have the most amount of {item}")
 
 def least(stock):
-    low = 100000000000000000000000
-    for quantity in stock.values():
-        if low > quantity:
-            low = quantity
-    for item, quantity in stock.items():
-        if quantity == low:
-            print(f"You have the least amount of {item}")
+    # low = 100000000000000000000000
+    # low = float("inf") is the better version of above and it means infinity
+
+    # for quantity in stock.values():
+    #     if low > quantity:
+    #         low = quantity
+
+    #gets the lowest amount of value
+    # low = min(stock.values())
+
+    # for item, quantity in stock.items():
+    #     if quantity == low:
+    #         print(f"You have the least amount of {item}")
+
+    item = min(stock, key=stock.get)
+    print(f"\tYou have the least amount of {item}")
 
 def get_name(prompt):
     while True:
@@ -161,11 +217,13 @@ def get_name(prompt):
 def get_int(prompt):
     while True:
         try:
-            amount = int(input(prompt))
-            return amount
+            return int(input(prompt))
         except ValueError:
             print("Invalid value.")
 
+def remove_empty(stock, name):
+    if stock[name] == 0:
+        del stock[name]
 
 if __name__ == "__main__":
     main()
